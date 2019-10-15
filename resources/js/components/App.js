1ea5f9c1b7d5503c78cdc3d2d26c5acf5283ebf8
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-10-15 16:44:27 +0200

    Adds basic router with placeholder components

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-    return (
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-    );
+const LandingPage = (props) => {
+    return <div>Landing Page</div>;
+};
+const LoginPage = (props) => {
+    return <div>Log In Page</div>;
+};
+const RegisterPage = (props) => {
+    return <div>Register Page</div>;
+};
+const DashboardPage = (props) => {
+    return <div>Dashboard Page</div>;
+};
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/register" component={RegisterPage} />
+                    <Route path="/dashboard" component={DashboardPage} />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
