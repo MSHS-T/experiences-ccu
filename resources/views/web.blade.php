ea01dcf98d2359e18882503e8952d805679bbb27
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-10-15 14:05:15 +0200

    Adds base skeleton for React in Laravel

---
+++ resources/views/web.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
+
+        <title>Expériences CCU</title>
+
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
