4d5607ab5070703ff77538791c8a9d109648b077
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-10-16 15:57:46 +0200

    Sets database structure for users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Insert admin user
+        DB::table('users')->insert([
+            'first_name' => 'Administrateur',
+            'last_name' => 'CCU',
+            'email' => 'romain@3rgo.tech',
+            'email_verified_at' => '2019-10-16 12:00',
+            'password' => bcrypt('admin'),
+            'created_at' => '2019-10-16 12:00',
+            'updated_at' => '2019-10-16 12:00'
+        ]);
+
         // $this->call(UsersTableSeeder::class);
     }
 }
