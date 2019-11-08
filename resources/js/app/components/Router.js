d83bc7bf335addb58b93795656d69700c069bbfd
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-11-08 19:57:53 +0100

    Reworks navigation and router to use an external sitemap array

--- resources/js/app/components/Router.js
+++ resources/js/app/components/Router.js
@@ -1,26 +1,49 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import intersection from 'lodash/intersection';
+
 import { useAuthContext } from "../context/Auth";
-import LandingPage from "../pages/Landing";
+import SiteMap from '../data/SiteMap';
+
 import LoginPage from "../pages/Login";
-import DashboardPage from "../pages/Dashboard";
-import UsersPage from "../pages/Users";
+import NotAuthorizedPage from "../pages/NotAuthorized";
 
-const PrivateRoute = ({ component, ...options }) => {
+const PrivateRoute = ({ component, roles, ...options }) => {
+    // Fetch user
     const { user } = useAuthContext();
-    const finalComponent = user ? component : LoginPage;
 
-    // TODO : Add authorization
+    // If user is empty (anonymous user), we will render the login page
+    let finalComponent = LoginPage;
+
+    if (user) {
+        // Else we render the given component
+        finalComponent = component;
+        // We check if there is at least 1 common role between the user's and the requirements
+        const isAllowed = intersection(user.roles, roles).length > 0;
+        if (!isAllowed) {
+            // If no matching roles are found, we render the NotAuthorized page
+            return <Route render={(props) => <NotAuthorizedPage {...props} requiredRoles={roles} />} {...options} />
+        }
+    }
+
     return <Route {...options} component={finalComponent} />;
 };
 
 const Router = () => (
     <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-
-        <PrivateRoute path="/dashboard" component={DashboardPage} />
-        <PrivateRoute path="/users" component={UsersPage} />
+        {/* We filter the dividers out */}
+        {SiteMap.filter(link => link !== '---').map((link, index) => {
+            const RouteType = link.authenticated ? PrivateRoute : Route;
+            return (
+                <RouteType
+                    key={index}
+                    path={link.url}
+                    exact={link.exactPath}
+                    roles={link.roles}
+                    component={link.component}
+                />
+            )
+        })}
     </Switch>
 );
 
