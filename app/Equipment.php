043f73e1b756a573011abcafde0186b1d86afdbd
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-01-10 19:22:09 +0100

    Fixes table name (equipment does not really have a plural form)

--- app/Equipment.php
+++ app/Equipment.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     /**
+     * The table name (equiment does not have a plural form)
+     *
+     * @var string
+     */
+    public $table = "equipments";
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
