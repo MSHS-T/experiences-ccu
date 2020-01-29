894f06499e74e574cf34bec6607d3c7a19e3356a
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-01-29 11:05:55 +0100

    Fixes API base url

--- resources/js/app/data/Constants.js
+++ resources/js/app/data/Constants.js
@@ -1,5 +1,5 @@
 // API endpoints
-export const API_URL = "http://localhost/api/";
+export const API_URL = "/api/";
 
 export const API_USERS_ENDPOINT = API_URL + "user/";
 export const API_EQUIPMENTS_ENDPOINT = API_URL + "equipment/";
