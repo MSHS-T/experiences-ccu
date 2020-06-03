3e06a64dbb8a9e945d8c05ebc444db6d2bd2db67
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-06-03 16:28:53 +0200

    Adds migration, model, controller and routes for slots

---
+++ app/Slot.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Slot extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'start',
+        'end',
+        'subject_first_name',
+        'subject_last_name',
+        'subject_email'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'start',
+        'end'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+
+    public function manipulation()
+    {
+        return $this->belongsTo('App\Manipulation', 'manipulation_id');
+    }
+}
