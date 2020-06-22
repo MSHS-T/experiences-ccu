4e33d36c09136ae9c9905d1e02d0f12f42596ce6
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-06-22 18:27:31 +0200

    Adds booking/unbooking method for Slot

--- app/Slot.php
+++ app/Slot.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Slot extends Model
 {
@@ -49,5 +51,40 @@ class Slot extends Model
     public function manipulation()
     {
         return $this->belongsTo('App\Manipulation', 'manipulation_id');
+    }
+
+    public function book(string $email, string $firstName, string $lastName)
+    {
+        // Fill data
+        $this->subject_email = $email;
+        $this->subject_first_name = $firstName;
+        $this->subject_last_name = $lastName;
+        // Add confirmation data
+        $this->subject_confirmed = false;
+        $this->subject_confirmation_code = Str::uuid();
+        $this->subject_confirm_before = Carbon::now()->addHours(Setting::get('booking_confirmation_delay', 24));
+
+        $this->save();
+
+        // TODO : send email to ask for confirmation
+
+    }
+
+    public function clearBooking(bool $sendNotification = true)
+    {
+        // Keep email if we need to send a notification
+        $email = $this->subject_email;
+        // Clear data
+        $this->subject_email = null;
+        $this->subject_first_name = null;
+        $this->subject_last_name = null;
+        $this->subject_confirmed = null;
+        $this->subject_confirmation_code = null;
+        $this->subject_confirm_before = null;
+        $this->save();
+
+        if ($sendNotification) {
+            // TODO : send email notification
+        }
     }
 }
