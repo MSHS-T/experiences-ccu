f90083e42154cebf94adcf4cf90700a544c86de6
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-06-22 12:59:01 +0200

    Refactor slot generation code into manipulation model

--- app/Http/Controllers/API/SlotController.php
+++ app/Http/Controllers/API/SlotController.php
@@ -51,61 +51,7 @@ class SlotController extends Controller
         // If a date was given we will only fill that day
         $toDate = $fromDate;
 
-        // Existing slots
-        $existingSlots = $manipulation->slots()->orderBy('start')->get();
-
-        // If no date was given, we generate until quota is met
-        if ($fromDate === false) {
-            // If no slots exist we start at the manipulation start date
-            if ($manipulation->slots->isEmpty()) {
-                $fromDate = $manipulation->start_date;
-            }
-            // If slots already exist, we start the day after the last slot
-            else {
-                $fromDate = $existingSlots->last()->start->addDay();
-            }
-        }
-
-        // Generate all slots if manipulation doesn't have any
-        // TODO : If manipulation has slots, we generate after the last one until quota is met
-        $hours = $manipulation->available_hours;
-        $duration = $manipulation->duration;
-
-        $current_date = new Carbon($fromDate);
-        $current_date->startOfDay();
-        $slots = [];
-        do {
-            $day = $current_date->shortEnglishDayOfWeek;
-            if ($hours[$day]['enabled'] === true) {
-                foreach (['am', 'pm'] as $ampm) {
-                    if ($hours[$day][$ampm] === true) {
-                        list($start_h, $start_m) = array_map('intval', explode(':', $hours[$day]['start_' . $ampm]));
-                        list($end_h, $end_m) = array_map('intval', explode(':', $hours[$day]['end_' . $ampm]));
-                        $current_date->setTime($start_h, $start_m, 0);
-                        $limit = $current_date->copy()->setTime($end_h, $end_m, 0);
-                        while ($current_date->diffInMinutes($limit) >= $duration) {
-                            $next_date = $current_date->copy()->addMinutes($duration);
-                            $slots[] = ['start' => $current_date, 'end' => $next_date];
-                            $current_date = $next_date->copy();
-                        }
-                    }
-                }
-            }
-            $current_date->startOfDay()->addDay();
-            if ($toDate !== false && $current_date->greaterThan(Carbon::create($toDate)->endOfDay())) {
-                break;
-            }
-        } while ((count($slots) + count($existingSlots)) < $manipulation->target_slots); // TODO : Use overbooking percentage from settings
-
-        // Filter slots against existing ones (if any) to avoid duplicates
-        $slots = array_filter($slots, function ($s) use ($existingSlots) {
-            return $existingSlots->isEmpty() || $existingSlots->every(function ($slot) use ($s) {
-                return $s['start']->greaterThanOrEqualTo($slot->end)
-                    || $s['end']->lessThanOrEqualTo($slot->start);
-            });
-        });
-
-        $manipulation->slots()->createMany($slots);
+        $manipulation->generateSlots($fromDate, $toDate);
 
         return $manipulation->slots()->orderBy('start')->get();
     }
