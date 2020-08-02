8b39425aeaa4d1fa9b3758535db04137e93b01a8
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-08-02 13:28:57 +0200

    Adds statistics for each manipulation on the dashboard

--- resources/js/app/pages/Dashboard.js
+++ resources/js/app/pages/Dashboard.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { GridList, Typography, Card, CardContent } from '@material-ui/core';
+import { GridList, Typography, Card, CardContent, Grid, Chip, Box, CircularProgress, withStyles } from '@material-ui/core';
 import * as moment from 'moment';
 import { useAuthContext } from '../context/Auth';
 import * as Constants from '../data/Constants';
 import Loading from './Loading';
+import { grey } from '@material-ui/core/colors';
 
 const useStyles = makeStyles(theme => ({
     root: {
         display:        'flex',
         flexWrap:       'wrap',
         justifyContent: 'space-around',
-        // overflow:        'hidden',
-        // backgroundColor: theme.palette.background.paper,
     },
     card: {
         width:  350,
         height: 300,
         margin: theme.spacing(2)
-    },
+    }
 }));
+
+const OutlinedCircularProgress = withStyles(() => ({
+    circleStatic: {
+        fill: grey[200],
+    },
+}))(CircularProgress);
 
 export default function Dashboard() {
     const classes = useStyles();
@@ -77,18 +82,56 @@ export default function Dashboard() {
             </Typography>
             <hr />
             <div className={classes.root}>
-                {data.map(manipulation => (
-                    <Card className={classes.card} key={manipulation.id}>
-                        <CardContent>
-                            <Typography variant="h5" component="h2">
-                                {manipulation.name}
-                            </Typography>
-                            <Typography variant="body1" component="div">
+                {data.map(manipulation => {
+                    const slotsBookedConfirmed = manipulation.slots.filter(s => s.booking !== null && s.booking.confirmed).length;
+                    const slotsBookedUnconfirmed = manipulation.slots.filter(s => s.booking !== null && !s.booking.confirmed).length;
+                    const honoredBookings = manipulation.slots.filter(s => s.booking !== null && s.booking.honored).length;
+                    const progress = Math.floor(Math.min(honoredBookings, manipulation.target_slots) / manipulation.target_slots * 100);
+                    return (
+                        <Card className={classes.card} key={manipulation.id}>
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    {manipulation.name}
+                                </Typography>
+                                <Typography variant="body1" component="div">
                                 Ouvert depuis le {moment(manipulation.start_date).format('DD/MM/YYYY')} ({moment().diff(manipulation.start_date, 'days')} jours)
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                ))}
+                                </Typography>
+                                <Box width={'100%'} display="flex" alignItems="center" justifyContent="center">
+                                    <Box position="relative" display="inline-flex">
+                                        <OutlinedCircularProgress variant="static" value={progress} size={160} />
+                                        <Box
+                                            top={0}
+                                            left={0}
+                                            bottom={0}
+                                            right={0}
+                                            position="absolute"
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="center"
+                                            flexDirection="column"
+                                        >
+                                            <Typography variant="h6" component="div" color="textPrimary">
+                                                {`${progress}%`}
+                                            </Typography>
+                                            <Typography variant="caption" component="div" color="textSecondary">
+                                                <center>
+                                                    <strong>{manipulation.target_slots}</strong> ciblés
+                                                    <br/>
+                                                    <strong>{manipulation.slots.length}</strong> créneaux
+                                                    <br/>
+                                                    <strong>{`${slotsBookedConfirmed} (+${slotsBookedUnconfirmed})`}</strong> inscriptions
+                                                    <br/>
+                                                    <strong>{honoredBookings}</strong> participants
+                                                    <br/>
+                                                    <br/>
+                                                </center>
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    ); })}
             </div>
         </>
     );
