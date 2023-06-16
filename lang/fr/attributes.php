6a0aff167ef46b936e0d434454f14bc1dbb7f3e5
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-06-16 13:52:30 +0200

    Add translations

--- lang/fr/attributes.php
+++ lang/fr/attributes.php
@@ -31,6 +31,17 @@ return [
     'requirements'    => 'Pré-requis',
     'published'       => 'Publié ?',
 
+    // Hours
+    'monday'    => 'Lundi',
+    'tuesday'   => 'Mardi',
+    'wednesday' => 'Mercredi',
+    'thursday'  => 'Jeudi',
+    'friday'    => 'Vendredi',
+    'start_am'  => 'Début de matinée',
+    'end_am'    => 'Fin de matinée',
+    'start_pm'  => 'Début d\'après-midi',
+    'end_pm'    => 'Fin d\'après-midi',
+
     // Settings
     'booking_cancellation_delay' => 'Délai de prévenance pour l\'annulation d\'une réservation',
     'booking_confirmation_delay' => 'Délai maximal pour la confirmation d\'une réservation',
