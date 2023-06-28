0eb18ed0de37959802ff3288d456d60e003dce90
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-06-28 17:53:50 +0200

    Adds birthdate to booking

--- database/migrations/2023_05_13_114337_create_bookings_table.php
+++ database/migrations/2023_05_13_114337_create_bookings_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
+            $table->date('birthday');
             $table->boolean('confirmed');
             $table->string('confirmation_code');
             $table->dateTime('confirm_before');
