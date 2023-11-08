fae758f9807f109c24c4670ffdd1c1b9a36aaddd
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-11-08 17:45:01 +0100

    Fixes slots display on the front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/fr'; // import locale
 import.meta.glob([
-    '../images/**',
-  ]);
+  '../images/**',
+]);
 
+dayjs.locale('fr'); // use locale
+window.dayjs = dayjs;
 window.Alpine = Alpine;
 
 Alpine.start();
