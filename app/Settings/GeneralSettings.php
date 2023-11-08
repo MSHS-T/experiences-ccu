ce88f96a4280c1a44e2a862c768221df26dc1233
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-11-08 16:02:07 +0100

    Remove unused settings ; Fixes settings page empty

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -9,7 +9,6 @@ class GeneralSettings extends Settings
     public int $booking_cancellation_delay;
     public int $booking_confirmation_delay;
     public int $booking_opening_delay;
-    public int $manipulation_overbooking;
     public int $email_reminder_delay;
     public string $presentation_text;
     public string $access_instructions;
