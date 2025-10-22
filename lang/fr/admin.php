998c61ea1b2f5854d8781edc533ca04b7aad7f0e
Romain GONÇALVES <romain.goncalves@gmail.com>
2025-10-22 11:58:23 +0200

    Fixes label

--- lang/fr/admin.php
+++ lang/fr/admin.php
@@ -7,7 +7,7 @@ return [
     'no_booking'                       => 'Aucune réservation',
     'plateau_planning'                 => 'Planning du Plateau ":name"',
     'manipulation_planning'            => 'Planning de la Manipulation ":name"',
-    'choose_manipulation'              => 'Choisir une manipulation',
+    'choose_manipulation'              => 'Choisir une expérience',
     'slot_creation_confirmation_intro' => 'Vous êtes sur le point de créer les créneaux suivants :',
     'slot_creation_confirmation_outro' => 'Veuillez vérifier les horaires avant la création.',
     'no_slot_creatable_for_interval'   => 'Aucun créneau ne peut être créé pour l\'intervalle de temps choisi',
